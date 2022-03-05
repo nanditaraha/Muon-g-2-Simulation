cd1d03c433d36510b67d501ae81b9d9ce96b844d
Nandita Raha <nandita.raha@gmail.com>
2022-03-05 06:22:22 -0500

    Add files via upload

---
+++ My_B1/src/my_B1ActionInitialization.cc
@@ -0,0 +1,70 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file my_B1ActionInitialization.cc
+/// \brief Implementation of the my_B1ActionInitialization class
+
+#include "my_B1ActionInitialization.hh"
+#include "my_B1PrimaryGeneratorAction.hh"
+#include "my_B1RunAction.hh"
+#include "my_B1EventAction.hh"
+#include "my_B1SteppingAction.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+my_B1ActionInitialization::my_B1ActionInitialization()
+ : G4VUserActionInitialization()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+my_B1ActionInitialization::~my_B1ActionInitialization()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void my_B1ActionInitialization::BuildForMaster() const
+{
+  my_B1RunAction* runAction = new my_B1RunAction;
+  SetUserAction(runAction);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void my_B1ActionInitialization::Build() const
+{
+  SetUserAction(new my_B1PrimaryGeneratorAction);
+
+  my_B1RunAction* runAction = new my_B1RunAction;
+  SetUserAction(runAction);
+  
+  my_B1EventAction* eventAction = new my_B1EventAction(runAction);
+  SetUserAction(eventAction);
+  
+  SetUserAction(new my_B1SteppingAction(eventAction));
+}  
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
